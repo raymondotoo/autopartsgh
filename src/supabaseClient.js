94d19d7cd5198cdacfef4448fc9b094800be5024
Raymond Otoo <raotoo@miners.utep.edu>
2026-03-14 20:50:53 -0700

    update secrets

---
+++ src/supabaseClient.js
@@ -0,0 +1,7 @@
+import { createClient } from '@supabase/supabase-js'
+
+// Access the environment variables from the build process
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey)
